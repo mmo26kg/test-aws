db243944a58e26442faa322c0fd3fd1c0378bf8c
DucNguyen <hongducmmo@gmail.com>
2025-08-23 00:03:48 +0700

    Update test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Chào mừng bạn đến với ứng dụng Node.js trên AWS Elastic Beanstalk!');
+    res.send('Chào mừng bạn đến với ứng dụng Node.js trên Nguyen Hong Duc!');
 });
 
 app.listen(port, () => {
